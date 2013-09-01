0cf27571b50638e5f4283efea9ba0aa802a9ad83
Lincoln de Sousa <lincoln@comum.org>
2013-09-01 02:43:39 -0400

    First steps towards a nice downloader

--- curdling/util.py
+++ curdling/util.py
@@ -1,9 +1,14 @@
 from __future__ import absolute_import, unicode_literals, print_function
 from pkg_resources import Requirement
+
+from pip.req import InstallRequirement
+from pip.index import PackageFinder
+
 import io
 import os
 import re
 
+import urllib2
 
 INCLUDE_PATTERN = re.compile(r'-r\s*\b([^\b]+)')
 
@@ -54,4 +59,33 @@ class Env(object):
             return False
 
         self.download_queue.put(requirement)
+        return False
+
+
+class PipSource(object):
+    def __init__(self, dirs=None, urls=None):
+        self.finder = PackageFinder(
+            find_links=dirs or [],
+            index_urls=urls or [])
+
+    def url(self, package):
+        return self.finder.find_requirement(
+            InstallRequirement.from_line(package),
+            True).url
+
+
+class DownloadManager(object):
+    def __init__(self, sources=None, storage=None):
+        self.sources = sources
+        self.storage = storage
+
+    def download(self, package_name, url):
+        pkg = urllib2.urlopen(url).read()
+        path = gen_package_path(package_name)
+        return self.storage.write(path, pkg)
+
+    def retrieve(self, package):
+        for source in self.sources:
+            pkg = self.download(package, source.url(package))
+            return pkg
         return False
