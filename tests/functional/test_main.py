e04a48c0d3a54366dd53ffb011e6112357c2ce09
Lincoln de Sousa <lincoln@comum.org>
2013-09-01 22:55:53 -0400

    Merging test_download_packages back to test_main

--- tests/functional/test_main.py
+++ tests/functional/test_main.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals, print_function
+from curdling.download import PipSource, DownloadManager, MemoryStorage
 from gevent.queue import Queue
 from mock import Mock
 import os
@@ -131,6 +132,38 @@ def test_service():
     package.should.equal('gherkin==0.1.0')
 
     my_mock.ran.should.equal('gherkin==0.1.0')
+
+
+def test_downloader():
+    "It should be possible to download packages from pip repos"
+
+    # Given the following downloader component
+    sources = [PipSource(urls=['http://localhost:8000/simple'])]
+    storage = MemoryStorage()
+    downloader = DownloadManager(sources=sources, storage=storage)
+
+    # When I try to retrieve a package from it
+    package = downloader.retrieve('gherkin==0.1.0')
+
+    # Then I see that the package was downloaded correctly to the storage
+    (package in storage).should.be.true
+
+
+def test_downloader_with_no_packages():
+    "After downloading packages, the result queue should be fed"
+
+    # Given the following downloader component
+    queue = Mock()
+    sources = [PipSource(urls=['http://localhost:8000/simple'])]
+    storage = MemoryStorage()
+    downloader = DownloadManager(
+        sources=sources, storage=storage, result_queue=queue)
+
+    # When I try to retrieve a package from it
+    package = downloader.retrieve('donotexist==0.1.0')
+
+    # Then I see package is none :(
+    package.should.be.none
 
 
 def test_curd_package():
