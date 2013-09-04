011d53f7495b13da074a92be083ad93be947cee4
Lincoln de Sousa <lincoln@comum.org>
2013-09-04 12:35:39 -0400

    New release: 0.1.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ install_requires, dependency_links = \
 if __name__ == '__main__':
     setup(
         name="curdling",
-        version='0.1.1',
+        version='0.1.2',
         description="curdling",
         long_description=local_file('README.md'),
         author='Lincoln Clarete',
