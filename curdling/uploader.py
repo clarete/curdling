27049b352934cf33b86013db677edf404f9569fb
Lincoln de Sousa <lincoln@comum.org>
2013-09-16 12:38:53 -0400

    Use urllib3 to PUT files on curdling servers. Closes #6
    With that change, we're just removing `requests` from our
    requirements.txt file.

--- curdling/uploader.py
+++ curdling/uploader.py
@@ -1,18 +1,29 @@
 from __future__ import absolute_import, unicode_literals, print_function
 from .service import Service
 
+import io
 import os
+import urllib3
 import urlparse
-import requests
 
 
 class Uploader(Service):
 
+    def __init__(self, *args, **kwargs):
+        super(Uploader, self).__init__(*args, **kwargs)
+        self.opener = urllib3.PoolManager()
+
     def handle(self, requester, package, sender_data):
+        # Preparing the url to PUT the file
         path = sender_data.pop('path')
         server = sender_data.pop('server')
         package = os.path.basename(path)
         url = urlparse.urljoin(server, 'p/{0}'.format(package))
-        with open(path, 'rb') as data:
-            requests.put(url, files={package: data}, data={package: package})
+
+        # Sending the file to the server. Both `method` and `url` parameters
+        # for calling `request_encode_body()` must be `str()` instances, not
+        # unicode.
+        contents = io.open(path, 'rb').read()
+        self.opener.request_encode_body(
+            b'PUT', bytes(url), {package: (package, contents)})
         return {'url': url}
