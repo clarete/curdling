e19005e906ca4aa11228fe672206f0bbe5ebd9fc
Lincoln de Sousa <lincoln@comum.org>
2013-09-27 01:13:33 -0400

    Move unit tests to their right place

--- tests/functional/test_main.py
+++ tests/functional/test_main.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, print_function, unicode_literals
-from mock import Mock, ANY
+from mock import Mock
 from nose.tools import nottest
 import os
 import errno
@@ -16,55 +16,6 @@ from curdling.services.curdler import Curdler
 from curdling.services.installer import Installer
 
 from . import FIXTURE
-
-
-def test_service():
-    "Service#_worker() should stop when hitting the sentinel"
-
-    # Given the following service
-    class MyService(Service):
-        pass
-
-    callback = Mock()
-    service = MyService()
-    service.connect('failed', callback)
-
-    # When I queue one package to be processed than I queue the stop sentinel
-    service.queue('main', 'package')
-    service.queue(None, None)
-    service._worker()
-
-    # Then I see that the package is indeed processed but the service dies
-    # properly when it receives the sentinel.
-    callback.assert_called_once_with('myservice', 'package', path=ANY)
-
-    # And that in the `path` parameter we receive an exception (Unfortunately
-    # we can't compare NotImplementedError() instances :(
-    callback.call_args_list[0][1]['path'].message.should.equal(
-        'The service subclass should override this method'
-    )
-
-
-def test_service_success():
-    "Service#_worker() should execute self#handler() method successfully"
-
-    # Given the following service
-    class MyService(Service):
-        def handle(self, requester, package, sender_data):
-            return {'package': 'processed-package'}
-
-    callback = Mock()
-    service = MyService()
-    service.connect('finished', callback)
-
-    # When I queue one package to be processed than I queue the stop sentinel
-    service.queue('main', 'package')
-    service.queue(None, None)
-    service._worker()
-
-    # Then I see that the right signal was emitted
-    callback.assert_called_once_with(
-        'myservice', 'package', package='processed-package')
 
 
 def test_downloader_with_no_sources():
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, print_function, unicode_literals
-from mock import call, patch, Mock
+from mock import call, patch, Mock, ANY
 from nose.tools import nottest
 
 import io
@@ -406,3 +406,55 @@ def test_signal_that_does_not_exist():
         AttributeError,
         'There is no such signal (clicked) in this emitter (button)',
     )
+
+
+# -- Signals --
+
+
+def test_service():
+    "Service#_worker() should stop when hitting the sentinel"
+
+    # Given the following service
+    class MyService(Service):
+        pass
+
+    callback = Mock()
+    service = MyService()
+    service.connect('failed', callback)
+
+    # When I queue one package to be processed than I queue the stop sentinel
+    service.queue('main', 'package')
+    service.queue(None, None)
+    service._worker()
+
+    # Then I see that the package is indeed processed but the service dies
+    # properly when it receives the sentinel.
+    callback.assert_called_once_with('myservice', 'package', path=ANY)
+
+    # And that in the `path` parameter we receive an exception (Unfortunately
+    # we can't compare NotImplementedError() instances :(
+    callback.call_args_list[0][1]['path'].message.should.equal(
+        'The service subclass should override this method'
+    )
+
+
+def test_service_success():
+    "Service#_worker() should execute self#handler() method successfully"
+
+    # Given the following service
+    class MyService(Service):
+        def handle(self, requester, package, sender_data):
+            return {'package': 'processed-package'}
+
+    callback = Mock()
+    service = MyService()
+    service.connect('finished', callback)
+
+    # When I queue one package to be processed than I queue the stop sentinel
+    service.queue('main', 'package')
+    service.queue(None, None)
+    service._worker()
+
+    # Then I see that the right signal was emitted
+    callback.assert_called_once_with(
+        'myservice', 'package', package='processed-package')
