clarete/curdling

commit e6812a5ae00dad17827a85a0150a252fd5b43488

Fix terminology confusion for package and requirement
The term `package` was being used in a lot of places where `requirement`
would fit better. Also, a couple `package` variables were renamed to
`package_name` ensure we're talking about a simple string, not an object
with more metadata or a gzip file.

curdling/services/downloader.py
@@ -26,9 +26,9 @@ def get_locator(conf):
     return AggregatingLocator(*(curds + pypi), scheme='legacy')
 
 
-def find_packages(locator, package, versions):
+def find_packages(locator, requirement, versions):
     scheme = distlib.version.get_scheme(locator.scheme)
-    matcher = scheme.matcher(package.requirement)
+    matcher = scheme.matcher(requirement.requirement)
 
     result = {}
     if versions:
@@ -64,9 +64,7 @@ class AggregatingLocator(locators.AggregatingLocator):
         pkg = util.parse_requirement(requirement)
         for locator in self.locators:
             versions = locator.get_project(pkg.name)
-            package = find_packages(locator, pkg, versions)
-            if package:
-                return package
+            return find_packages(locator, pkg, versions) or None
 
 
 class PyPiLocator(locators.SimpleScrapingLocator):
@@ -150,7 +148,7 @@ class CurdlingLocator(locators.Locator):
         self.base_url = url
         self.url = url
         self.opener = Pool(maxsize=POOL_MAX_SIZE)
-        self.packages_not_found = []
+        self.requirements_not_found = []
 
     def get_distribution_names(self):
         return json.loads(
@@ -169,7 +167,7 @@ class CurdlingLocator(locators.Locator):
             data = json.loads(response.data)
             return {v['version']: self._get_distribution(v) for v in data}
         else:
-            self.packages_not_found.append(name)
+            self.requirements_not_found.append(name)
 
     def _get_distribution(self, version):
         # Source url for the package
@@ -194,28 +192,30 @@ class Downloader(Service):
         self.opener = Pool(maxsize=POOL_MAX_SIZE)
         self.locator = get_locator(self.conf)
 
-    def handle(self, requester, package, sender_data):
+    def handle(self, requester, requirement, sender_data):
         found = None
         prereleases = self.conf.get('prereleases', True)
 
-        # It sounds lame, but we're trying to match packages with more than one
-        # word separated with either `_` or `-`. Notice that we prefer hyphens
-        # cause theres currently way more packages using hyphens than
+        # It sounds lame, but we're trying to match requirements with more than
+        # one word separated with either `_` or `-`. Notice that we prefer
+        # hyphens cause theres currently way more packages using hyphens than
         # underscores in pypi.p.o. Let's wait for the best here.
-        for option in [package.replace('_', '-'), package.replace('-', '_')]:
+        options = requirement.replace('_', '-'), requirement.replace('-', '_')
+        for option in options:
             found = self.locator.locate(option, prereleases)
             if found:
                 break
 
         if not found:
-            raise ReportableError('Package `{0}\' not found'.format(package))
+            raise ReportableError('Requirement `{0}\' not found'.format(
+                requirement))
         return {"path": self.download(found)}
 
     def get_servers_to_update(self):
         failures = {}
         for locator in self.locator.locators:
-            if isinstance(locator, CurdlingLocator) and locator.packages_not_found:
-                failures[locator.base_url] = locator.packages_not_found
+            if isinstance(locator, CurdlingLocator) and locator.requirements_not_found:
+                failures[locator.base_url] = locator.requirements_not_found
         return failures
 
     # -- Private API of the Download service --

curdling/database.py
@@ -9,15 +9,16 @@ import os
 class Database(object):
 
     @classmethod
-    def check_installed(cls, package):
-        return DistributionPath(include_egg=True).get_distribution(
-            util.safe_name(util.parse_requirement(package).name)) is not None
+    def check_installed(cls, requirement):
+        path = DistributionPath(include_egg=True)
+        package_name = util.safe_name(util.parse_requirement(requirement).name)
+        return path.get_distribution(package_name) is not None
 
     @classmethod
-    def uninstall(self, package):
+    def uninstall(self, requirement):
         # Currently we assume the distribution path contains only the last
         # version installed
-        package_name = util.safe_name(util.parse_requirement(package).name)
+        package_name = util.safe_name(util.parse_requirement(requirement).name)
         distribution = DistributionPath(include_egg=True).get_distribution(
             package_name)
 

curdling/install.py
@@ -27,15 +27,15 @@ PACKAGE_BLACKLIST = (
 
 def only(func, pattern):
     @wraps(func)
-    def wrapper(requester, package, **data):
+    def wrapper(requester, requirement, **data):
         if re.match(pattern, data.get('path', '')):
-            return func(requester, package, **data)
+            return func(requester, requirement, **data)
     return wrapper
 
 
 def mark(maestro, set_name):
-    def marker(requester, package, **data):
-        return maestro.mark(set_name, package, data.get('path'))
+    def marker(requester, requirement, **data):
+        return maestro.mark(set_name, requirement, data.get('path'))
     return marker
 
 
@@ -84,8 +84,8 @@ class Install(object):
     def report(self):
         if self.maestro.failed:
             print('\nSome milk was spilled in the process:')
-        for package in self.maestro.failed:
-            _, version = self.maestro.best_version(package)
+        for package_name in self.maestro.failed:
+            _, version = self.maestro.best_version(package_name)
             data = version.get('data')
             print(" * {0}: {1}".format(data.__class__.__name__, data))
 
@@ -113,9 +113,9 @@ class Install(object):
         else:
             self.installer.start()
 
-        for package in self.maestro.mapping:
-            _, version = self.maestro.best_version(package)
-            self.installer.queue('main', package, path=version['data'])
+        for package_name in self.maestro.mapping:
+            _, version = self.maestro.best_version(package_name)
+            self.installer.queue('main', package_name, path=version['data'])
 
         ui = InstallProgress(self, 'installed')
         while ui:
@@ -138,40 +138,41 @@ class Install(object):
             return SUCCESS
 
         uploader = self.uploader.start()
-        for server, packages in failures.items():
-            for package in packages:
-                _, data = self.maestro.best_version(package)
-                uploader.queue('main', package,
+        for server, package_names in failures.items():
+            for package_name in package_names:
+                _, data = self.maestro.best_version(package_name)
+                uploader.queue('main', package_name,
                     path=data.get('data'), server=server)
         uploader.join()
         return SUCCESS
 
-    def request_install(self, requester, package, **data):
+    def request_install(self, requester, requirement, **data):
         # If it's a blacklisted requirement, we should cowardly refuse to
         # install
         for blacklisted in PACKAGE_BLACKLIST:
-            if package.startswith(blacklisted):
+            if requirement.startswith(blacklisted):
                 self.logger.info(
                     "Cowardly refusing to install blacklisted "
-                    "requirement `%s'", package)
+                    "requirement `%s'", requirement)
                 return False
 
-        # Well, the package is installed, let's just bail
-        if not self.conf.get('force') and self.database.check_installed(package):
+        # Well, the requirement is installed, let's just bail
+        if not self.conf.get('force') and self.database.check_installed(requirement):
             return True
 
-        # We shouldn't queue the same package twice
-        if not self.maestro.should_queue(package):
+        # We shouldn't queue the same requirement twice
+        if not self.maestro.should_queue(requirement):
             return False
 
         # Let's tell the maestro we have a new challenger
-        self.maestro.file_package(package, dependency_of=data.get('dependency_of'))
+        self.maestro.file_requirement(
+            requirement, dependency_of=data.get('dependency_of'))
 
         # Looking for built packages
         try:
-            path = self.index.get("{0};whl".format(package))
-            self.dependencer.queue(requester, package, path=path)
-            self.maestro.mark('retrieved', package, 'whl')
+            path = self.index.get("{0};whl".format(requirement))
+            self.dependencer.queue(requester, requirement, path=path)
+            self.maestro.mark('retrieved', requirement, 'whl')
             return False
         except PackageNotFound:
             pass
@@ -179,15 +180,15 @@ class Install(object):
         # Looking for downloaded packages. If there's packages of any of the
         # following distributions, we'll just build the wheel
         try:
-            path = self.index.get("{0};~whl".format(package))
-            self.curdler.queue(requester, package, path=path)
-            self.maestro.mark('retrieved', package, 'compressed')
+            path = self.index.get("{0};~whl".format(requirement))
+            self.curdler.queue(requester, requirement, path=path)
+            self.maestro.mark('retrieved', requirement, 'compressed')
             return False
         except PackageNotFound:
             pass
 
         # Nops, we really don't have the package
-        self.downloader.queue(requester, package, **data)
+        self.downloader.queue(requester, requirement, **data)
         return False
 
 

curdling/maestro.py
@@ -37,26 +37,26 @@ class Maestro(object):
 
         self.lock = threading.RLock()
 
-    def file_package(self, package, dependency_of=None):
-        requirement = util.parse_requirement(package)
+    def file_requirement(self, requirement, dependency_of=None):
+        requirement = util.parse_requirement(requirement)
         version = constraints(requirement)
 
         with self.lock:
             entry = self.mapping[util.safe_name(requirement.name)][version]
             entry['dependency_of'].append(dependency_of)
 
-    def get_data(self, package):
-        requirement = util.parse_requirement(package)
+    def get_data(self, requirement):
+        requirement = util.parse_requirement(requirement)
         version = constraints(requirement)
         return self.mapping[util.safe_name(requirement.name)][version]['data']
 
-    def set_data(self, package, data):
-        pkg = util.parse_requirement(package)
-        version = constraints(pkg)
-        self.mapping[util.safe_name(pkg.name)][version]['data'] = data
+    def set_data(self, requirement, data):
+        requirement = util.parse_requirement(requirement)
+        version = constraints(requirement)
+        self.mapping[util.safe_name(requirement.name)][version]['data'] = data
 
-    def mark(self, attr, package, data):
-        pkg = util.parse_requirement(package)
+    def mark(self, attr, requirement, data):
+        pkg = util.parse_requirement(requirement)
         getattr(self, attr).add(util.safe_name(pkg.name))
 
         # The 'installed' label doesn't actually need to save any data, so we
@@ -65,13 +65,13 @@ class Maestro(object):
         # packages. Needed to find the right bucket inside of the
         # project_name+version sub-dictionary structure.
         if data is not None:
-            self.set_data(package, data)
+            self.set_data(requirement, data)
 
         # Since we couldn't install this package, we should also mark its
         # requesters as failed too.
         if attr == 'failed':
-            for parent in filter(None, self.get_parents(package)):
-                self.mark('failed', parent, BrokenDependency(package))
+            for parent in filter(None, self.get_parents(requirement)):
+                self.mark('failed', parent, BrokenDependency(requirement))
 
     def get_parents(self, spec):
         requirement = util.parse_requirement(spec)
@@ -134,8 +134,8 @@ class Maestro(object):
         return versions[0]
 
     def should_queue(self, requirement):
-        parsed_requirement = util.parse_requirement(requirement)
-        package_name = util.safe_name(parsed_requirement.name)
+        requirement = util.parse_requirement(requirement)
+        package_name = util.safe_name(requirement.name)
 
         # There might be people trying to write in our mapping and we always
         # need the updated values.

curdling/services/base.py
@@ -31,10 +31,11 @@ class Service(SignalEmitter):
         self.finished = Signal()
         self.failed = Signal()
 
-    def queue(self, requester, package, **data):
-        self._queue.put((requester, package, data))
-        self.logger.info(' * queue(from=%s, to=%s, package=%s, data=%s)',
-            requester, self.name, package, data)
+    def queue(self, requester, requirement, **data):
+        self._queue.put((requester, requirement, data))
+        self.logger.info(
+            'queue(from="%s", to="%s", requirement="%s", data="%s")',
+            requester, self.name, requirement, data)
         return self
 
     def start(self):
@@ -59,7 +60,7 @@ class Service(SignalEmitter):
             worker.join()
         self.workers = []
 
-    def handle(self, requester, package, sender_data):
+    def handle(self, requester, requirement, sender_data):
         raise NotImplementedError(
             "The service subclass should override this method")
 
@@ -68,26 +69,26 @@ class Service(SignalEmitter):
     def _worker(self):
         # If the service consumer invokes `.queue(None, None)` it causes the
         # worker to die elegantly by matching the following sentinel:
-        for requester, package, sender_data in iter(self._queue.get, SENTINEL):
-            self.logger.debug(' * %s[%s].run(package=%s, sender_data=%s)',
+        for requester, requirement, sender_data in iter(self._queue.get, SENTINEL):
+            self.logger.debug('%s[%s].run(requirement="%s", sender_data="%s")',
                 self.name, threading.current_thread().name,
-                package, sender_data)
+                requirement, sender_data)
             try:
-                self.emit('started', self.name, package, **sender_data)
-                data = self.handle(requester, package, sender_data)
+                self.emit('started', self.name, requirement, **sender_data)
+                data = self.handle(requester, requirement, sender_data)
                 self._queue.task_done()
             except ReportableError as exc:
-                self.emit('failed', self.name, package, path=exc)
-                self.logger.info(" # %s.error(): %s", self.name, exc)
+                self.emit('failed', self.name, requirement, path=exc)
+                self.logger.info("%s.error(): %s", self.name, exc)
                 self.logger.exception(
-                    'failed to run %s (requested by:%s) for package %s:',
-                    self.name, requester, package)
+                    'failed to run %s (requested by:%s) for requirement %s:',
+                    self.name, requester, requirement)
             except BaseException as exc:
-                self.emit('failed', self.name, package, path=exc)
+                self.emit('failed', self.name, requirement, path=exc)
                 self.logger.exception(
-                    'failed to run %s (requested by:%s) for package %s:',
-                    self.name, requester, package)
+                    'failed to run %s (requested by:%s) for requirement %s:',
+                    self.name, requester, requirement)
             else:
-                self.emit('finished', self.name, package, **(data or {}))
-                self.logger.info(' * %s.result(package=%s): %s ... OK',
-                    self.name, package, data)
+                self.emit('finished', self.name, requirement, **(data or {}))
+                self.logger.info('%s.result(requirement="%s"): %s ... OK',
+                    self.name, requirement, data)

curdling/services/curdler.py
@@ -116,7 +116,7 @@ def unpack(package, destination):
 
 class Curdler(Service):
 
-    def handle(self, requester, package, sender_data):
+    def handle(self, requester, requirement, sender_data):
         source = sender_data.pop('path')
 
         # Place used to unpack the wheel
@@ -130,6 +130,7 @@ class Curdler(Service):
             wheel_file = setup_py('bdist_wheel')
             return {'path': self.index.from_file(wheel_file)}
         except BaseException as exc:
-            raise BuildError('{0}: {1}'.format(package, spaces(3, str(exc))))
+            raise BuildError('{0}: {1}'.format(
+                requirement, spaces(3, str(exc))))
         finally:
             shutil.rmtree(destination)

curdling/services/dependencer.py
@@ -11,7 +11,7 @@ class Dependencer(Service):
         self.dependency_found = Signal()
         self.built = Signal()
 
-    def handle(self, requester, package, sender_data):
+    def handle(self, requester, requirement, sender_data):
         # Find the wheel
         path = sender_data.pop('path')
         wheel = Wheel(path)
@@ -21,8 +21,8 @@ class Dependencer(Service):
             # Packages might declare their "extras" here, so let's split it
             dependency, extra = (';' in spec and spec or spec + ';').split(';')
             self.emit('dependency_found', self.name,
-                dependency, dependency_of=package)
+                dependency, dependency_of=requirement)
         else:
-            self.emit('built', self.name, package, path=path)
+            self.emit('built', self.name, requirement, path=path)
 
         return {'deps': run_time_dependencies}

curdling/services/installer.py
@@ -37,8 +37,8 @@ def get_distribution_paths(name):
 
 class Installer(Service):
 
-    def handle(self, requester, package, sender_data):
+    def handle(self, requester, requirement, sender_data):
         source = sender_data.pop('path')
-        name = parse_requirement(package).name
+        name = parse_requirement(requirement).name
         wheel = Wheel(source)
         wheel.install(get_distribution_paths(name))

commit 202294088df892ed279824600c3f9ea87b1e0cbb

AggregatingLocator.locate should try the next locator if the first one fails
This stupid bug was caused in "e6812a5ae00dad17827a85a0150a252fd5b43488" during
a terminology change.

curdling/services/downloader.py
@@ -80,7 +80,9 @@ class AggregatingLocator(locators.AggregatingLocator):
         pkg = util.parse_requirement(requirement)
         for locator in self.locators:
             versions = locator.get_project(pkg.name)
-            return find_packages(locator, pkg, versions) or None
+            packages = find_packages(locator, pkg, versions)
+            if packages:
+                return packages
 
 
 class PyPiLocator(locators.SimpleScrapingLocator):