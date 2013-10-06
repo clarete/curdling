bf60198ccc0f19b447e017fea3472206e22c91d2
Lincoln de Sousa <lincoln@comum.org>
2013-10-06 19:34:26 -0400

    Add tests for the Dependencer service

---
+++ tests/unit/test_services_dependencer.py
@@ -0,0 +1,49 @@
+from mock import Mock, patch
+from curdling.services.dependencer import Dependencer
+
+
+@patch('curdling.services.dependencer.Wheel')
+def test_dependencer(Wheel):
+    "Dependencer#handle() should emit the signal dependency_found when scanning a new package"
+
+    # Given that I have the depencencer service
+    callback = Mock()
+    dependencer = Dependencer()
+    dependencer.connect('dependency_found', callback)
+
+    # And that the package to test the service will have the following
+    # dependencies:
+    Wheel.return_value = Mock(metadata=Mock(requires_dist=['forbiddenfruit (0.1.1)']))
+
+    # When I queue a package and a sentinel and then call the worker
+    dependencer.queue('tests', 'sure', path='path-to-the-wheel')
+    dependencer.queue(None, None)
+    dependencer._worker()
+
+    # Than I see that the signal was called for the dependency with the right
+    # parameters
+    callback.assert_called_once_with(
+        'dependencer', u'forbiddenfruit (0.1.1)', dependency_of='sure')
+
+
+@patch('curdling.services.dependencer.Wheel')
+def test_dependencer_package_with_no_deps(Wheel):
+    "Dependencer#handle() should emit the signal built for packages with no dependencies"
+
+    # Given that I have the depencencer service
+    callback = Mock()
+    dependencer = Dependencer()
+    dependencer.connect('built', callback)
+
+    # And that the package to test the service will have the following
+    # dependencies:
+    Wheel.return_value = Mock(metadata=Mock(requires_dist=[]))
+
+    # When I queue a package and a sentinel and then call the worker
+    dependencer.queue('tests', 'sure', path='path-to-the-wheel')
+    dependencer.queue(None, None)
+    dependencer._worker()
+
+    # Than I see that the signal was called for the dependency with the right
+    # parameters
+    callback.assert_called_once_with('dependencer', 'sure', path='path-to-the-wheel')
