6db19e8390fee56ebd5046155a799bdc50ec4d72
Lincoln de Sousa <lincoln@comum.org>
2013-10-08 16:03:20 -0400

    Remove another old maestro test and fix test_file_requirement()

--- tests/unit/test_maestro.py
+++ tests/unit/test_maestro.py
@@ -12,14 +12,11 @@ def test_file_requirement():
     # When I file a requirement
     maestro.file_requirement('curdling')
 
-    # Then I see that the requirement was filed under the default status
-    maestro.status('curdling').should.equal(Maestro.PENDING)
-
-    # And then I see that the mapping attribute has all the data we need to
-    # process a package
+    # Then I see that the mapping attribute has all the data we need to process
+    # a requirement
     maestro.mapping.should.equal({
         'curdling': {
-            'status': Maestro.IDLE,
+            'status': Maestro.PENDING,
             'dependency_of': [None],
             'data': {
                 'directory': None,
@@ -283,38 +280,6 @@ def test_is_primary_requirement():
 # ---------------------- Here ----------------------
 
 
-def test_maestro_pending_packages_no_deps():
-    "It shoudl be possible to mark packages as built maestro"
-
-    # Given that I have a maestro with a package filed under it
-    maestro = Maestro()
-    maestro.file_requirement('curdling', dependency_of=None)
-
-    # When and I mark the package as `checked`,
-    # meaning that all the dependencies were checked
-    maestro.mark('built', 'curdling', {})
-
-    # Then I see it's still waiting for the dependency checking
-    maestro.pending('built').should.equal([])
-    maestro.built.should.equal(set(['curdling']))
-
-
-def test_mark_failed():
-    "It shoudl be possible to mark packages as failed in the maestro"
-
-    # Given that I have a maestro with a package filed under it
-    maestro = Maestro()
-    maestro.file_requirement('curdling', dependency_of=None)
-
-    # When and I mark the package as `failed`, meaning that all the
-    # dependencies were checked
-    maestro.mark('failed', 'curdling', {})
-
-    # Then I see it's still waiting for the dependency checking
-    maestro.pending('built').should.equal([])
-    maestro.failed.should.equal(set(['curdling']))
-
-
 def test_get_parents():
     "Maestro#get_parents() should return a list of requesters of a given package"
 
@@ -330,118 +295,6 @@ def test_get_parents():
 
     # Then I see both packages that depend on `urllib3` were returned
     parents.should.equal(['curdling', 'requests'])
-
-
-def test_marking_parent_packages_as_failed_when_a_dependency_fails():
-    "Packages should be marked as failed when one or more of its dependencies can't be built"
-
-    # Given that I have a maestro with a package and a dependency filed
-    maestro = Maestro()
-    maestro.file_requirement('curdling', dependency_of=None)
-    maestro.file_requirement('urllib3', dependency_of='curdling')
-
-    # When I mark the package `urllib3` as failed
-    maestro.mark('failed', 'urllib3', {'exception': Exception('P0wned!!!')})
-
-    # Then I see the `curdling` package was also marked as failed
-    maestro.failed.should.equal(set(['curdling', 'urllib3']))
-
-    # And then I see that we don't have any packages pending
-    maestro.pending('built').should.be.empty
-
-
-def test_mark_built_update_mapping():
-
-    # Given that I have a maestro with a couple packages filed under it
-    maestro = Maestro()
-    maestro.file_requirement('curdling', dependency_of=None)
-    maestro.file_requirement('sure (== 0.1.2)', dependency_of='curdling')
-    maestro.file_requirement('forbiddenfruit (> 0.1.0)', dependency_of='curdling')
-    maestro.file_requirement('forbiddenfruit (>= 0.1.2)', dependency_of='sure (== 0.1.2)')
-
-    # Wehn I mark the files as built
-    maestro.mark('built', 'curdling', {'path': '/curds/curdling-0.3.5.whl'})
-    maestro.mark('built', 'sure (== 0.1.2)', {'path': '/curds/sure-0.1.2.whl'})
-    maestro.mark('built', 'forbiddenfruit (> 0.1.0)', {'path': '/curds/forbiddenfruit-0.1.2.whl'})
-    maestro.mark('built', 'forbiddenfruit (>= 0.1.2)', {'path': '/curds/forbiddenfruit-0.1.2.whl'})
-
-    # Then I see I still have just one entry in the mapping
-    dict(maestro.mapping).should.equal({
-        'curdling': {
-            None: {
-                'dependency_of': [None],
-                'data': {'path': '/curds/curdling-0.3.5.whl'},
-            },
-        },
-        'sure': {
-            '== 0.1.2': {
-                'dependency_of': ['curdling'],
-                'data': {'path': '/curds/sure-0.1.2.whl'},
-            },
-        },
-        'forbiddenfruit': {
-            '> 0.1.0': {
-                'dependency_of': ['curdling'],
-                'data': {'path': '/curds/forbiddenfruit-0.1.2.whl'},
-            },
-            '>= 0.1.2': {
-                'dependency_of': ['sure (== 0.1.2)'],
-                'data': {'path': '/curds/forbiddenfruit-0.1.2.whl'},
-            },
-        },
-    })
-
-
-def test_mark_installed():
-    "Maestro should be able to track installed packages"
-
-    # Given a maestro with a few packages
-    maestro = Maestro()
-    maestro.file_requirement('curdling')
-    maestro.file_requirement('sure')
-
-    # When I mark `curdling` as installed
-    maestro.mark('installed', 'sure', data=None)
-
-    # Then I see that the package was marked as installed
-    maestro.installed.should.equal(set(['sure']))
-
-
-def test_should_queue():
-    "Maestro#should_queue should not allow repeated packages in the maestro"
-
-    # Given that I have an empty maestro
-    maestro = Maestro()
-
-    # When I check if I can queue a package that is *not* present in the
-    # maestro instance, Then I see it returns true
-    maestro.should_queue('curdling').should.be.true
-
-    # After filing this package to the maestro, should_queue will change its
-    # results, as you can see here.
-    maestro.file_requirement('curdling')
-    maestro.should_queue('curdling').should.be.false
-
-
-def test_get_data():
-    "It should be possible to retrieve data of a given requirement"
-
-    # Given that I have a maestro filled with a package
-    maestro = Maestro()
-    maestro.mapping = {
-        'forbiddenfruit': {
-            '> 0.1.0': {
-                'dependency_of': [],
-                'data': {'path': '/curds/forbiddenfruit.whl'},
-            },
-        }
-    }
-
-    # When I retrieve the data from the following requirement
-    data = maestro.get_data('forbiddenfruit (> 0.1.0)')
-
-    # Then I see the correct value retrieved
-    data.should.equal({'path': '/curds/forbiddenfruit.whl'})
 
 
 def test_best_version():
@@ -533,8 +386,8 @@ def test_best_version_no_strict_requirements_but_strict_version():
     })
 
 
-def test_best_version_no_direct_req():
-    "best_version() with no direct requirements"
+def test_best_version_dependency():
+    "Maestro#best_version() should work for dependencies as well"
 
     # Given that I have a maestro with a package that contains more than one
     # version, but none directly requested by the user
@@ -554,37 +407,6 @@ def test_best_version_no_direct_req():
         'dependency_of': ['luxury (== 0.1.1)'],
         'data': {'path': '/curds/forbiddenfruit-0.1.1-cp27.whl'},
     })
-
-
-def test_best_version_should_prefer_newest_version():
-    "Maestro#best_version should prefer newest versions when there's no override"
-
-    # Given that I have a couple versions of the same package but all of them
-    # were requested by some other package
-    maestro = Maestro()
-    maestro.mapping = {
-        'forbiddenfruit': {
-            '>= 0.0.9': {
-                'dependency_of': ['sure (== 0.2)'],
-                'data': {'path': '/curds/forbiddenfruit-0.1.0-cp27-none-macosx_10_8_x86_64.whl'},
-            },
-            '<= 0.1.8': {
-                'dependency_of': ['luxury (== 0.1.0)'],
-                'data': {'path': '/curds/forbiddenfruit-0.1.8-cp27-none-macosx_10_8_x86_64.whl'},
-            },
-            '>= 0.1.7': {
-                'dependency_of': ['luxury (== 0.1.0)'],
-                'data': {'path': '/curds/forbiddenfruit-0.1.7-cp27-none-macosx_10_8_x86_64.whl'},
-            },
-        }
-    }
-
-    maestro.best_version('forbiddenfruit').should.equal(
-        ('<= 0.1.8', {
-            'dependency_of': ['luxury (== 0.1.0)'],
-            'data': {'path': '/curds/forbiddenfruit-0.1.8-cp27-none-macosx_10_8_x86_64.whl'},
-        })
-    )
 
 
 def test_best_version_should_blow_up_on_version_conflicts():
