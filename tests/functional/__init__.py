622b2ab33b7be5abca615bc3a5d4c3f90d25b0e6
Lincoln de Sousa <lincoln@comum.org>
2013-10-28 01:31:46 -0400

    make deploy-docs at your service

--- tests/functional/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import, print_function, unicode_literals
-import os
-
-# Those are functional tests, I need to do some IO and all the files I have are
-# located in the `fixtures` folder. The following line is just a shortcut to
-# build absolute paths pointing to things inside of that folder.
-FIXTURE = lambda *p: os.path.join(os.path.dirname(__file__), 'fixtures', *p)
