clarete/curdling

commit c2321a1ddf3a00c677a3e51c7d5c9474da699ff0

Allow both lower case and upper case log levels in the command line

curdling/tool/__init__.py
@@ -215,7 +215,7 @@ def main():
     levels = [i for i in logging._levelNames.keys()
         if not isinstance(i, int) and i != 'NOTSET']
     parser.add_argument(
-        '-l', '--log-level', default='CRITICAL', choices=levels,
+        '-l', '--log-level', default='CRITICAL', choices=levels, type=unicode.upper,
         help='Log verbosity level (for nerds): {0}'.format(', '.join(levels)))
 
     parser.add_argument(

commit 3de75806b2ba4c8c7fe5ad127a69cbb4e1d1bba3

Fix unicode reference
It breaks python3 support because `unicode` is not a valid name in python3.
The bug was introduced in c2321a1

curdling/tool/__init__.py
@@ -236,7 +236,7 @@ def main():
         levels = [i for i in logging._levelToName.keys()
             if not isinstance(i, int) and i != 'NOTSET']
     parser.add_argument(
-        '-l', '--log-level', default='CRITICAL', choices=levels, type=unicode.upper,
+        '-l', '--log-level', default='CRITICAL', choices=levels, type=type('').upper,
         help='Log verbosity level (for nerds): {0}'.format(', '.join(levels)))
 
     parser.add_argument(