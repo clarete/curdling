27c160ea0adad2771bee12c48a57208a1a3c4d9a
Lincoln de Sousa <lincoln@comum.org>
2017-12-17 09:29:07 -0500

    tool: Another attempt to convert log level to uppercase properly

--- curdling/tool/__init__.py
+++ curdling/tool/__init__.py
@@ -236,7 +236,8 @@ def main():
         levels = [i for i in logging._levelToName.keys()
             if not isinstance(i, int) and i != 'NOTSET']
     parser.add_argument(
-        '-l', '--log-level', default='CRITICAL', choices=levels, type=type('').upper,
+        '-l', '--log-level', default='CRITICAL', choices=levels,
+        type=lambda x: x.upper(),
         help='Log verbosity level (for nerds): {0}'.format(', '.join(levels)))
 
     parser.add_argument(
